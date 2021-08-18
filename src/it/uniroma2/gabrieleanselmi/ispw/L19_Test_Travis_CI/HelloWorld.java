f27a1555423df30527c1efdca82a9ccb150803a6
GabryLele98 <46486689+GabryLele98@users.noreply.github.com>
2021-08-18 17:29:22 +0200

    #1 - HelloWorld project created

---
+++ src/it/uniroma2/gabrieleanselmi/ispw/L19_Test_Travis_CI/HelloWorld.java
@@ -0,0 +1,9 @@
+package it.uniroma2.gabrieleanselmi.ispw.L19_Test_Travis_CI;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
