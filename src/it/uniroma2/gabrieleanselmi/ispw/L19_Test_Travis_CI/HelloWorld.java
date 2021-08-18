059317f5a6f663e68217a64e8aef92a675de9d59
GabryLele98 <46486689+GabryLele98@users.noreply.github.com>
2021-08-18 17:55:27 +0200

    #4 - bug created

--- src/it/uniroma2/gabrieleanselmi/ispw/L19_Test_Travis_CI/HelloWorld.java
+++ src/it/uniroma2/gabrieleanselmi/ispw/L19_Test_Travis_CI/HelloWorld.java
@@ -3,7 +3,7 @@ package it.uniroma2.gabrieleanselmi.ispw.L19_Test_Travis_CI;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+		System.out.println("Hello World!")
+	
 
 }
